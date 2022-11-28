8dc635aa94572203f668a5c79d08d244034c5631
oj2302 <72265255+oj2302@users.noreply.github.com>
2022-11-28 22:57:13 +0000

    fix(PlayerMovement.cs): Fixed player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerRB;
 
-    private float moveSpeed = 5000f;
+    private float moveSpeed = 50f;
     
     private float xAxisInput;
     private float zAxisInput;
 
-    private void Update()
+    private void FixedUpdate()
     {
         GetPlayerInput();
         MovePlayer();
@@ -26,6 +26,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
-        playerRB.velocity = new Vector3(xAxisInput * moveSpeed * Time.deltaTime, playerRB.velocity.y, zAxisInput * moveSpeed * Time.deltaTime);
+        playerRB.velocity += transform.right * xAxisInput * moveSpeed * Time.fixedDeltaTime + transform.forward * zAxisInput * moveSpeed * Time.fixedDeltaTime;
     }
 }
